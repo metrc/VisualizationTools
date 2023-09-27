0101d52c88f13742258d76c5d773a4bc43ae245d
Jyoti Dubey <jyotidubey505@gmail.com>
2023-09-27 15:36:42 -0400

    Adding function to calculate mean & standard devitaion and fixed the formatting for calculating percentage

--- R/value_formatting.R
+++ R/value_formatting.R
@@ -14,5 +14,24 @@
 #' format_count_percent()
 #' }
 format_count_percent <- function(numerator, denominator, decimals=0){
-  return(paste0(numerator,' (', round(numerator / denominator * 100, decimals), '%)'))
+  return(paste0(numerator,' (', format(round(numerator / denominator * 100, decimals), nsmall = decimals), '%)'))
+}
+
+
+#' Format Mean Standand Deviation
+#' 
+#' @description Returns the mean and standard deviation for the requested vector(numbers)
+#' 
+#' @param values insert values that you request mean and standard deviation for
+#' @param decimals number of decimals places, defaults to zero
+#' 
+#' @return string
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' format_mean_sd()
+#' }
+format_mean_sd <- function(values, decimals = 2){
+  return(paste0(format(round(mean(values, na.rm = TRUE),decimals), nsmall = decimals), " (", format(round(sd(values, na.rm = TRUE),decimals), nsmall = decimals), ")"))
 }
